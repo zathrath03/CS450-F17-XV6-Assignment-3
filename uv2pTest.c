e003ac1d0058c2c202e433db2950ff8f56a9b444
John Dugger <jdugger@iit.edu>
2017-11-06 19:52:13 -0600

    fixed uv2pTest's return statement

--- uv2pTest.c
+++ uv2pTest.c
@@ -7,6 +7,6 @@ int main (void)
 {
 	int *virtAddress = (int*)12285; //limited to 12285 2^14-2^12
 	uv2p(virtAddress);
-	printf(1, "back in ps.c\n");
+	printf(1, "back in uv2pTest.c\n");
 	exit();
 }
